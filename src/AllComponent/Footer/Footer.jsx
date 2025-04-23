b831210235c21f063d20838276f238feeaf9e073
minazul01 <minajul4010@gmail.com>
2025-04-23 13:18:00 +0600

    all done

--- src/AllComponent/Footer/Footer.jsx
+++ src/AllComponent/Footer/Footer.jsx
@@ -1,70 +1,83 @@
+import React from "react";
 
-import { FaSquareXTwitter } from "react-icons/fa6";
+// react icons
+import { CgFacebook } from "react-icons/cg";
+import {BsLinkedin, BsTwitter } from "react-icons/bs";
 import { FaYoutube } from "react-icons/fa";
-import { FaFacebook } from "react-icons/fa";
-import { FaCcVisa } from "react-icons/fa";
-import { FaCcPaypal } from "react-icons/fa";
-import { FaCcApplePay } from "react-icons/fa";
+
+import { SlArrowUp } from "react-icons/sl";
 import { Link } from "react-router-dom";
 
 const Footer = () => {
-    return (
-        <div>
-            <footer className="footer footer-horizontal footer-center bg-base-200 text-base-content rounded p-10">
-                <nav className="grid grid-flow-col gap-4">
-                    <a className="link link-hover">About us</a>
-                    <Link className="link link-hover" to="/contact">Contact Us</Link>
-                    <a className="link link-hover">Press kit</a>
-                </nav>
-                <nav>
-                    <div className="grid grid-flow-col gap-4">
-                        <a
-                            href="https://twitter.com/yourprofile"
-                            target="_blank"
-                            rel="noopener noreferrer"
+  return (
+    <footer className="bg-white shadow-md rounded-xl w-full p-3 md:p-4 relative">
+      <div className="w-full flex items-center justify-center pt-[30px] flex-col gap-[20px] pb-[130px]">
+        <img
+          src="https://i.ibb.co/ZHYQ04D/footer-logo.png"
+          alt="logo"
+          className="w-[150px]"
+        />
 
-                        >
-                            <FaSquareXTwitter className="w-12 h-8" />
-                        </a>
+        <p className="text-[0.9rem] text-center sm:text-start text-gray-600">
+          High level experience in web design and development knowledge,
+          producing quality work.
+        </p>
 
-                        <a
-                            href="https://www.youtube.com/@gollachut2491"
-                            target="_blank"
-                            rel="noopener noreferrer"
+        <Link to="/contact">
+          <button className="py-3 px-6 rounded-full bg-[#3B9DF8] text-white">
+            Contact Us
+          </button>
+        </Link>
 
-                        >
-                            <FaYoutube className="w-12 h-8" />
-                        </a>
-
-                        <a
-                            href="https://www.facebook.com/yourpage"
-                            target="_blank"
-                            rel="noopener noreferrer"
-
-                        >
-                            <FaFacebook className="w-12 h-8" />
-                        </a>
-                    </div>
-                </nav>
-                <div className="flex gap-10">
-                    <a href="https://bd.visa.com/" target="_blank" rel="noopener noreferrer">
-                        <FaCcVisa className="text-3xl font-bold"></FaCcVisa>
-                    </a>
-                    <a href="https://www.paypal.com/bd/home" target="_blank" rel="noopener noreferrer">
-
-                        <FaCcPaypal className="text-3xl font-bold"></FaCcPaypal>
-                    </a>
-                    <a href="https://www.ipay.com.bd/" target="_blank" rel="noopener noreferrer">
-
-                        <FaCcApplePay className="text-3xl font-bold"></FaCcApplePay>
-                    </a>
-                </div>
-                <aside>
-                    <p>Copyright © {new Date().getFullYear()} - All right reserved by ACME Industries Ltd</p>
-                </aside>
-            </footer>
+        <div className="flex gap-[15px] text-black mt-4">
+          <a
+            className="text-[1.3rem] p-1.5 cursor-pointer rounded-full bg-white text-[#424242] shadow-md"
+            href="https://www.facebook.com/minazul.islam.38795"
+            target="_blank"
+          >
+            <CgFacebook />
+          </a>
+          <a className="text-[1.2rem] p-1.5 cursor-pointer rounded-full bg-white text-[#424242] shadow-md"
+          href="https://x.com/Minazul01"
+          target="_blank"
+          >
+            <BsTwitter />
+          </a>
+          <a className="text-[1.2rem] p-1.5 cursor-pointer rounded-full bg-white text-[#424242] shadow-md"
+          href="https://www.youtube.com/"
+          target="_blank"
+          >
+            <FaYoutube />
+          </a>
+          <a className="text-[1.2rem] p-1.5 cursor-pointer rounded-full bg-white text-[#424242] shadow-md"
+          href="https://www.linkedin.com/in/minazul-islam-2371972b5/"
+          target="_blank"
+          >
+            <BsLinkedin />
+          </a>
         </div>
-    );
+      </div>
+
+      <div className="z-30 absolute bottom-3 left-0 right-0 px-3 flex items-center justify-between w-full">
+        <p className="text-[0.9rem] text-gray-300">
+          © 2021 All Rights Reserved
+        </p>
+
+        <SlArrowUp className="p-2 rounded-full border border-gray-300 cursor-pointer text-[2rem] text-gray-300" />
+      </div>
+
+      <img
+        src="https://i.ibb.co/zNk7XT4/Rectangle-97.png"
+        alt="background/image"
+        className="absolute bottom-[20px] sm:bottom-0 left-0 right-0 z-10 rounded-b-xl"
+      />
+      <img
+        src="https://i.ibb.co/0mp2FwS/Rectangle-95.png"
+        alt="background/image"
+        className="absolute bottom-0 left-0 right-0 z-10 rounded-b-xl"
+      />
+    </footer>
+  );
 };
 
 export default Footer;
--- src/AllComponent/Main/Contact.jsx
+++ src/AllComponent/Main/Contact.jsx
@@ -1,29 +1,143 @@
+import React from "react";
 
+// react icons
+import { MdOutlineCall, MdOutlineEmail } from "react-icons/md";
+import { IoLocationOutline } from "react-icons/io5";
+import { CgFacebook } from "react-icons/cg";
+import { BsLinkedin, BsTwitter } from "react-icons/bs";
+import { FaYoutube } from "react-icons/fa";
+import { toast, ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
-const Contact = () => {
-    return (
+const ContactForm = () => {
+  const handleSent = (e) => {
+    e.preventDefault();
+    const formData = new FormData(e.target.closest("form"));
+    const email = formData.get("email");
+    if (!email) {
+      toast.error("Please enter your email address!");
+      return;
+    }
+
+    toast.success("Message sent successfully!");
+  };
+  return (
+    <section className="w-full grid grid-cols-1 md:grid-cols-2 gap-[35px] boxShadow p-[30px] rounded-xl my-10">
+      {/*  informations  */}
+      <aside className="w-full bg-gray-800 flex flex-col justify-between p-[25px] rounded-md">
         <div>
-            <div className="hero bg-base-200 min-h-screen">
-                <div className="">
-                    <div className=" bg-base-100 w-full max-w-[1450px] rounded-2xl shrink-0 shadow-2xl">
-                        <div className="card-body">
-                            <fieldset className="fieldset w-fit md:w-[400px] h-fit md:h-[300px] flex-col items-center justify-center">
-                                <label className="label">Name</label>
-                                <input type="name" className="input" placeholder="Name" />
-                                <label className="label">Email</label>
-                                <input type="Email" className="input" placeholder="Email" />
-                                
-                                <label className="label">Phone</label>
-                                <input type="Phone" className="input" placeholder="Phone" />
-                                
-                                <button className="btn btn-neutral mt-4">Sent</button>
-                            </fieldset>
-                        </div>
-                    </div>
-                </div>
-            </div>
+          <h1 className="text-[2rem] font-[600] leading-[35px] text-white">
+            Contact Information
+          </h1>
+          <p className="text-[0.9rem] mt-1 mb-8 text-white">
+            Say something to start a live chat!
+          </p>
         </div>
-    );
+
+        <div className="flex flex-col gap-[20px] text-gray-300">
+          <p className="flex items-center gap-[8px]">
+            <MdOutlineCall />
+            +8801305282768
+          </p>
+          <p className="flex items-center break-all gap-[8px]">
+            <MdOutlineEmail />
+            zenuilibrary@gmail.com
+          </p>
+          <p className="flex items-center gap-[8px]">
+            <IoLocationOutline />
+            Kulaura, Moulvibazar, Sylhet
+          </p>
+        </div>
+
+        <div className="flex gap-[15px] flex-wrap text-black mt-8">
+          <a
+            href="https://www.facebook.com/minazul.islam.38795"
+            target="_blank"
+            className="text-[1.2rem] p-1.5 cursor-pointer rounded-full bg-orange-500 text-white hover:bg-white hover:text-orange-500 transition-all duration-300 boxShadow"
+          >
+            <CgFacebook />
+          </a>
+
+          <a
+            className="text-[1.2rem] p-1.5 cursor-pointer rounded-full  bg-orange-500 text-white hover:bg-white hover:text-orange-500 transition-all duration-300 boxShadow"
+            href="https://x.com/Minazul01"
+            target="_blank"
+          >
+            <BsTwitter />
+          </a>
+          <a className="text-[1.2rem] p-1.5 cursor-pointer rounded-full bg-orange-500 text-white hover:bg-white hover:text-orange-500 transition-all duration-300  boxShadow"
+          href="https://www.youtube.com/"
+          target="_blank"
+          >
+            <FaYoutube />
+          </a>
+          <a className="text-[1.2rem] p-1.5 cursor-pointer rounded-full  bg-orange-500 text-white hover:bg-white hover:text-orange-500 transition-all duration-300 boxShadow"
+          href="https://www.linkedin.com/in/minazul-islam-2371972b5/"
+          target="_blank"
+          >
+            <BsLinkedin />
+          </a>
+        </div>
+      </aside>
+
+      {/* form area */}
+      <form className="pt-[20px]">
+        <div className="flex flex-col sm:flex-row items-center gap-[30px]">
+          <div className="flex flex-col gap-[5px] w-full sm:w-[50%]">
+            <label className="text-[1rem] text-gray-700">First Name</label>
+            <input
+              type="text"
+              className="peer border-gray-300 border-b outline-none focus:border-[#3B9DF8] w-full text-gray-400 transition-colors duration-300"
+            />
+          </div>
+
+          <div className="flex flex-col gap-[5px] w-full sm:w-[50%]">
+            <label className="text-[1rem] text-gray-700">Last Name</label>
+            <input
+              type="text"
+              className="peer border-gray-300 border-b outline-none focus:border-[#3B9DF8] w-full text-gray-400 transition-colors duration-300"
+            />
+          </div>
+        </div>
+
+        <div className="flex flex-col sm:flex-row items-center gap-[30px] mt-10">
+          <div className="flex flex-col gap-[5px] w-full sm:w-[50%]">
+            <label className="text-[1rem] text-gray-700">Email Address</label>
+            <input
+              type="email"
+              name="email"
+              required
+              className="peer border-gray-300 border-b outline-none focus:border-[#3B9DF8] w-full text-gray-400 transition-colors duration-300"
+            />
+          </div>
+
+          <div className="flex flex-col gap-[5px] w-full sm:w-[50%]">
+            <label className="text-[1rem] text-gray-700">Phone Number</label>
+            <input
+              type="number"
+              className="peer border-gray-300 border-b outline-none focus:border-[#3B9DF8] w-full text-gray-400 transition-colors duration-300"
+            />
+          </div>
+        </div>
+
+        <div className="flex flex-col gap-[5px] w-full mt-10">
+          <label className="text-[1rem] text-gray-700">Write Message</label>
+          <textarea className="peer min-h-[100px] border-gray-300 border-b resize-none outline-none w-full text-gray-400 transition-colors focus:border-[#3B9DF8] duration-300"></textarea>
+        </div>
+
+        <div className="w-full flex items-center sm:items-end justify-center sm:justify-end mt-5">
+          <button
+            onClick={handleSent}
+            type="submit"
+            className="dark:border-slate-700 py-2.5 px-6 bg-gray-800 border transition-all duration-300 hover:border-gray-800 hover:text-gray-800 hover:bg-transparent text-white rounded-md text-[1rem] mt-[10px] w-max"
+          >
+            Send Message
+          </button>
+        </div>
+        <ToastContainer />
+      </form>
+    </section>
+  );
 };
 
-export default Contact;
+export default ContactForm;
