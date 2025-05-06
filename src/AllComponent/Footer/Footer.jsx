7148081f82cfd0ab2f8d7c7ce91c3717e3e1bef2
minazul01 <minajul4010@gmail.com>
2025-05-06 09:30:16 +0600

    update project

--- src/AllComponent/Footer/Footer.jsx
+++ src/AllComponent/Footer/Footer.jsx
@@ -2,26 +2,22 @@ import React from "react";
 
 // react icons
 import { CgFacebook } from "react-icons/cg";
-import {BsLinkedin, BsTwitter } from "react-icons/bs";
+import { BsLinkedin, BsTwitter } from "react-icons/bs";
 import { FaYoutube } from "react-icons/fa";
 
 import { SlArrowUp } from "react-icons/sl";
 import { Link } from "react-router-dom";
 
-
 const Footer = () => {
   return (
     <footer className="bg-white shadow-md rounded-xl w-full p-3 md:p-4 relative">
       <div className="w-full flex items-center justify-center pt-[30px] flex-col gap-[20px] pb-[130px]">
-        <img
-          src="https://i.ibb.co/ZHYQ04D/footer-logo.png"
-          alt="logo"
-          className="w-[150px]"
-        />
+        <h3 className="text-4xl font-bold text-[#3B9DF8]">Mountain-Treks</h3>
 
-        <p className="text-[0.9rem] text-center sm:text-start text-gray-600">
-          High level experience in web design and development knowledge,
-          producing quality work.
+        <p className="w-2/4 text-[1rem] sm:text-start text-gray-600">
+          Mountain treks offer thrilling adventures through rugged landscapes,
+          combining physical challenge with breathtaking views of nature’s
+          highest peaks.
         </p>
 
         <Link to="/contact">
@@ -38,21 +34,24 @@ const Footer = () => {
           >
             <CgFacebook />
           </a>
-          <a className="text-[1.2rem] p-1.5 cursor-pointer rounded-full bg-white text-[#424242] shadow-md"
-          href="https://x.com/Minazul01"
-          target="_blank"
+          <a
+            className="text-[1.2rem] p-1.5 cursor-pointer rounded-full bg-white text-[#424242] shadow-md"
+            href="https://x.com/Minazul01"
+            target="_blank"
           >
             <BsTwitter />
           </a>
-          <a className="text-[1.2rem] p-1.5 cursor-pointer rounded-full bg-white text-[#424242] shadow-md"
-          href="https://www.youtube.com/"
-          target="_blank"
+          <a
+            className="text-[1.2rem] p-1.5 cursor-pointer rounded-full bg-white text-[#424242] shadow-md"
+            href="https://www.youtube.com/"
+            target="_blank"
           >
             <FaYoutube />
           </a>
-          <a className="text-[1.2rem] p-1.5 cursor-pointer rounded-full bg-white text-[#424242] shadow-md"
-          href="https://www.linkedin.com/in/minazul-islam-2371972b5/"
-          target="_blank"
+          <a
+            className="text-[1.2rem] p-1.5 cursor-pointer rounded-full bg-white text-[#424242] shadow-md"
+            href="https://www.linkedin.com/in/minazul-islam-2371972b5/"
+            target="_blank"
           >
             <BsLinkedin />
           </a>
